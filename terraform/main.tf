5137bcea114d6b35bff964d69b9609c90cd92aaa
ahsan rasheed <sm.rash1001@gmail.com>
2026-07-26 22:36:29 -0400

    Add S3 static website hosting for dashboard via Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "dashboard" {
+  bucket = "finops-showback-dashboard-sn"
+}
+
+resource "aws_s3_bucket_website_configuration" "dashboard" {
+  bucket = aws_s3_bucket.dashboard.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "dashboard" {
+  bucket = aws_s3_bucket.dashboard.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "dashboard" {
+  bucket = aws_s3_bucket.dashboard.id
+  depends_on = [aws_s3_bucket_public_access_block.dashboard]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.dashboard.arn}/*"
+      }
+    ]
+  })
+}
+
+output "dashboard_url" {
+  value = "http://${aws_s3_bucket_website_configuration.dashboard.website_endpoint}"
+}
